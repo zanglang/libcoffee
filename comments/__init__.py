e9f97fc28e243875aaad08a29afc7a4754f404c0
zanglang <zanglang@gmail.com>
2009-09-28 00:08:43 +0800

    - Use Typepad for spam checking - app settings for previous few commits

--- comments/__init__.py
+++ comments/__init__.py
@@ -1,6 +1,9 @@
+from django.conf import settings
 from django.contrib.comments.models import Comment
+from django.contrib.sites.models import Site
 from django.core.mail import send_mail
 from django.db.models.signals import post_save
+from akismet import Akismet
 from comments.forms import CustomCommentForm
 from django.contrib.comments.signals import comment_was_posted
 
@@ -8,22 +11,20 @@ def get_form():
 	return CustomCommentForm
 
 def new_comment_notify(sender, comment, request, *args, **kwargs):
+	if request.user.is_staff: return # don't bother to notify
 	subject = 'New comment on %s' % comment.content_object.title
 	message = '%s wrote:\n\n%s' % (comment.user_name, comment.comment)
+	print 'sending mail for new comment'
 	send_mail(subject, message, 'noreply@libcoffee.net', ['admin@libcoffee.net'])
 	
 def new_comment_check(sender, comment, request, *args, **kwargs):
-	from django.contrib.sites.models import Site
-	from django.conf import settings
-	try:
-		from akismet import Akismet
-	except:
-		return
-	
+	if request.user.is_staff: return # don't bother to check
 	ak = Akismet(
-		key = settings.AKISMET_API_KEY,
-		blog_url = 'http://%s/' % Site.objects.get(pk = settings.SITE_ID).domain
+		#key = settings.AKISMET_API_KEY,
+		key = settings.TYPEPAD_API_KEY,
+		blog_url = 'http://%s/' % Site.objects.get_current().domain
 	)
+	ak.baseurl = 'api.antispam.typepad.com/1.1/'
 	if ak.verify_key():
 		data = {
 			'user_ip': request.META.get('REMOTE_ADDR', '127.0.0.1'),
@@ -35,13 +36,15 @@ def new_comment_check(sender, comment, request, *args, **kwargs):
 			'comment_author_email': comment.user_email.encode('utf-8'),			
 		}
 		if ak.comment_check(comment.comment.encode('utf-8'), data=data, build_data=True):
-			print('marking comment as spam')
+			print 'marking comment as spam'
 			comment.flags.create(
 				user=comment.content_object.author,
 				flag='spam'
 			)
 			comment.is_public = False
 			comment.save()
+		else:
+			print 'comment is ham'
 
 
 comment_was_posted.connect(new_comment_check, dispatch_uid='comments-spam')
